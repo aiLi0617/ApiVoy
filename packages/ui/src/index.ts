7b5179247db7d8dbb73f572fb6e9add8e5b8cb2b
renguoqiang <reguoqiang@zfnyunshang.com>
2026-08-12 19:03:55 +0800

    refactor(explorer): export flat tree model

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -5,7 +5,8 @@ export { FeedbackProvider, useFeedback, EmptyState, LoadingState } from "./Feedb
 export { useAppStore, type ThemeMode, type WorkbenchLayoutPreference, type SplitPanePreference } from "./appStore";
 export { VirtualList, getVirtualRange, type VirtualListProps, type VirtualRange } from "./VirtualList";
 export { ApiVoyProviders } from "./ApiVoyProviders";
-export { WorkspaceExplorer, type WorkspaceTree, type WorkspaceExplorerProps } from "./WorkspaceExplorer";
+export { WorkspaceExplorer, type WorkspaceTree, type WorkspaceExplorerProps, type WorkspaceRecord, type ProjectRecord, type CollectionRecord, type RequestRecord } from "./WorkspaceExplorer";
+export { flattenWorkspaceTree, type FlattenWorkspaceTreeOptions, type WorkspaceTreeRow } from "./workspaceTreeRows";
 export { SseWorkbench, type SseWorkbenchProps, type SseWorkbenchRequest } from "./SseWorkbench";
 export { SocketWorkbench, type SocketWorkbenchProps, type SocketWorkbenchRequest } from "./SocketWorkbench";
 export { GraphqlWorkbench, type GraphqlWorkbenchProps, type GraphqlWorkbenchRequest } from "./GraphqlWorkbench";
